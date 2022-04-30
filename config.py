3eb3480c891f3bf10440a574d58cf9dbd8595a14
粟俊 <sujun@bestsec.cn>
2022-04-30 20:00:45 +0800

    fix:数据库连接配置

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ DEBUG = False  # 开发
 # password = os.environ.get("MYSQL_PASSWORD", 'root')
 # db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
 
-# 微信云外网-微信开发者工具调试
-# username = os.environ.get("MYSQL_USERNAME", 'root')
-# password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
-# db_address = os.environ.get(
-#     "MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-n8ldyi7o.sql.tencentcdb.com:28135')
-
-# 微信云内网-线上
+# 微信云外网
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
-db_address = os.environ.get("MYSQL_ADDRESS", '10.0.224.4:3306')
+db_address = os.environ.get(
+    "MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-n8ldyi7o.sql.tencentcdb.com:28135')
+
+# 微信云内网
+# username = os.environ.get("MYSQL_USERNAME", 'root')
+# password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
+# db_address = os.environ.get("MYSQL_ADDRESS", '10.0.224.4:3306')
