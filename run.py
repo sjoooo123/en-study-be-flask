4fee48065c957f9392d3767f06f4e2884d72ccb5
粟俊 <sujun@bestsec.cn>
2022-04-30 20:15:27 +0800

    fix:部署地址修改为线上

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import sys
 from wxcloudrun import app
 # 启动Flask Web服务
 if __name__ == '__main__':
-    # app.run(host=sys.argv[1], port=sys.argv[2]) # 线上
-    app.run(host='127.0.0.1', port=5000)  # 开发
+    app.run(host=sys.argv[1], port=sys.argv[2])  # 线上
+    # app.run(host='127.0.0.1', port=5000)  # 开发
