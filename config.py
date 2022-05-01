83feabd0995b333954dfc8afd80fab2c420aa8cc
粟俊 <sujun@bestsec.cn>
2022-05-01 11:24:11 +0800

    fix:微信云外网切换到内网

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ DEBUG = False  # 开发
 # db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
 
 # 微信云外网
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
-db_address = os.environ.get(
-    "MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-n8ldyi7o.sql.tencentcdb.com:28135')
-
-# 微信云内网
 # username = os.environ.get("MYSQL_USERNAME", 'root')
 # password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
-# db_address = os.environ.get("MYSQL_ADDRESS", '10.0.224.4:3306')
+# db_address = os.environ.get(
+#     "MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-n8ldyi7o.sql.tencentcdb.com:28135')
+
+# 微信云内网
+username = os.environ.get("MYSQL_USERNAME", 'root')
+password = os.environ.get("MYSQL_PASSWORD", 'SJ1964swwhj')
+db_address = os.environ.get("MYSQL_ADDRESS", '10.0.224.4:3306')
